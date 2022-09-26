d08a421dcc316e28474ce2e5aeebe6c21ed2c890
Changhyung <pck4949@naver.com>
2022-09-27 02:43:31 +0900

    :lipstick: 회원가입 성공 화면 UI 추가

---
+++ app/src/main/java/com/softsquared/niceduck/android/sparky/view/sign_up/SignUpSuccessActivity.kt
@@ -0,0 +1,13 @@
+package com.softsquared.niceduck.android.sparky.view.sign_up
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.softsquared.niceduck.android.sparky.R
+import com.softsquared.niceduck.android.sparky.databinding.ActivitySignUpSuccessBinding
+import com.softsquared.niceduck.android.sparky.utill.BaseActivity
+
+class SignUpSuccessActivity : BaseActivity<ActivitySignUpSuccessBinding>(ActivitySignUpSuccessBinding::inflate) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
