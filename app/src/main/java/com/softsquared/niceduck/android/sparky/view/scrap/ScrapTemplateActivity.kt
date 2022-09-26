d3efd216e43f2b6af50c92ff956249f25f225ee7
Changhyung <pck4949@naver.com>
2022-09-27 00:43:13 +0900

    :art: 스크랩 기능 코드 구조 개선

--- app/src/main/java/com/softsquared/niceduck/android/sparky/view/scrap/ScrapTemplateActivity.kt
+++ app/src/main/java/com/softsquared/niceduck/android/sparky/view/scrap/ScrapTemplateActivity.kt
@@ -6,6 +6,8 @@ import android.os.Bundle
 import android.os.Parcelable
 import android.util.Log.d
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.TransformationUtils.centerCrop
+import com.softsquared.niceduck.android.sparky.R
 import com.softsquared.niceduck.android.sparky.databinding.ActivityScrapTemplateBinding
 import com.softsquared.niceduck.android.sparky.utill.BaseActivity
 import kotlinx.coroutines.CoroutineScope
@@ -96,7 +98,16 @@ class ScrapTemplateActivity : BaseActivity<ActivityScrapTemplateBinding>(Activit
 
     suspend fun setDataView(text: String, url: String, ogMap: Map<String, String>) {
         withContext(Dispatchers.Main) {
-            Glide.with(this@ScrapTemplateActivity).load(ogMap.get("image")).centerCrop().into(binding.scrapTemplateImgThumbnail)
+            if (ogMap.get("image").isNullOrEmpty()) {
+                if (intent.getStringExtra("ogImage").isNullOrEmpty()) {
+                    // Glide 옵션 fitCenter() or centerCrop()
+                    Glide.with(this@ScrapTemplateActivity).load(getDrawable(R.drawable.sparky)).fitCenter().into(binding.scrapTemplateImgThumbnail)
+                } else {
+                    Glide.with(this@ScrapTemplateActivity).load(intent.getStringExtra("ogImage")).fitCenter().into(binding.scrapTemplateImgThumbnail)
+                }
+            } else {
+                Glide.with(this@ScrapTemplateActivity).load(ogMap.get("image")).fitCenter().into(binding.scrapTemplateImgThumbnail)
+            }
             if (ogMap.get("title").isNullOrEmpty()) {
                 binding.scrapTemplateTxtTitle.setText(text)
             } else {
