a85aa8d5bbafab6dfdd79d38552a1fdf2fdb8050
Changhyung <pck4949@naver.com>
2022-09-27 02:09:14 +0900

    :lipstick: 회원가입 프레임 화면 UI 추가

---
+++ app/src/main/java/com/softsquared/niceduck/android/sparky/view/sign_up/SignUpActivity.kt
@@ -0,0 +1,13 @@
+package com.softsquared.niceduck.android.sparky.view.sign_up
+
+import android.os.Bundle
+import com.softsquared.niceduck.android.sparky.databinding.ActivitySignUpBinding
+import com.softsquared.niceduck.android.sparky.utill.BaseActivity
+
+class SignUpActivity : BaseActivity<ActivitySignUpBinding>(ActivitySignUpBinding::inflate) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
