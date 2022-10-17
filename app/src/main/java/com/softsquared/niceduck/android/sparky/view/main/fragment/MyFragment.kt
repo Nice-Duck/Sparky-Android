851521a5c06282af6947d7b1e96bb7b81a913573
Changhyung <pck4949@naver.com>
2022-10-18 02:26:18 +0900

    :lipstick: 마이 화면 프로토타입 UI 추가 (정렬 버튼의 경우 이미지 수정할 예정)

--- app/src/main/java/com/softsquared/niceduck/android/sparky/view/main/fragment/MyFragment.kt
+++ app/src/main/java/com/softsquared/niceduck/android/sparky/view/main/fragment/MyFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.RadioButton
 import androidx.activity.viewModels
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
@@ -29,10 +30,15 @@ class MyFragment  :
             mainViewModel.getMyScrapAdapter2().submitList(it)
         })
 
+        binding.myRadioBtn2.isChecked = true
+
         val layoutManager1 = LinearLayoutManager(activity, LinearLayoutManager.VERTICAL, false)
         with(binding.myRecyclerview) {
             this.layoutManager = layoutManager1
             adapter = mainViewModel.getMyScrapAdapter2()
         }
+
+        
     }
+
 }
